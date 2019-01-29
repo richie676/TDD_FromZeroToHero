d5f69ce6789092b9e3fc0bc596878501eac7c6f7
Richard.Winterhalder <richard.winterhalder@anecon.com>
2019-01-29 14:31:49 +0100

    Add number support

---
+++ TDD_ROT13/RotEncryptor.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Text;
+
+namespace TDD_ROT13
+{
+    internal class RotEncryptor
+    {
+        private int ShiftBy = 13;
+        private const int LengthOfAlphabet = 26;
+        private const char LastLetterOfAlphabet = 'Z';
+        private const int LengthOfDigits = 10;
+        private const char LastDigit = '9';
+
+        public RotEncryptor()
+        {
+        }
+
+        public RotEncryptor(int shiftBy)
+        {
+            ShiftBy = shiftBy;
+        }
+
+        internal string Encrypt(string input)
+        {
+            var characters = PrepareForEncryption(input).ToCharArray();
+            StringBuilder encryptedValue = new StringBuilder();
+            foreach (var character in characters)
+            {
+                encryptedValue.Append(EncrypCharacter(character));
+            }
+
+            return encryptedValue.ToString();
+        }
+
+        private string PrepareForEncryption(string input)
+        {
+            string uppercaseInput = input.ToUpper();
+            uppercaseInput = uppercaseInput.Replace("Ä", "AE")
+                                           .Replace("Ö", "OE")
+                                           .Replace("Ü", "UE")
+                                           .Replace("ß", "SS");
+            return uppercaseInput;
+        }
+
+        private char EncrypCharacter(char character)
+        {
+            if (Char.IsLetter(character))
+            {
+                character = Shift(character, LengthOfAlphabet, LastLetterOfAlphabet);
+            }
+            else if (Char.IsDigit(character))
+            {
+                character = Shift(character, LengthOfDigits, LastDigit);
+            }
+
+            return character;
+        }
+
+        private char Shift(char character, int lengthLimit, char lastCharacter)
+        {
+            character = ShiftCharacter(character, ShiftBy % lengthLimit);
+
+            if (character > lastCharacter)
+            {
+                character = ShiftCharacter(character, -lengthLimit);
+            }
+
+            return character;
+        }
+
+        private char ShiftCharacter(char character, int shiftBy)
+        {
+            return (char)(character + shiftBy);
+        }
+    }
+}
---
+++ TDD_ROT13/RotEncryptorTest.cs
@@ -0,0 +1,94 @@
+using FluentAssertions;
+using System;
+using System.Text;
+using Xunit;
+
+namespace TDD_ROT13
+{
+    public class RotEncryptorTest
+    {
+
+        /*
+            A -> N
+            B -> O
+            a -> N
+            dEf -> QRS 
+            xyz -> KLM
+            Hello, World ->  URYYB, JBEYQ
+            Großraumbüroklimagerätestörung -> TEBFFENHZOHREBXYVZNTRENRGRFGBREHAT
+        */
+        [Fact]
+        public void ShouldShiftCharacters()
+        {
+            RotEncryptor("A").Should().Be("N");
+            RotEncryptor("B").Should().Be("O");
+        }
+
+        [Fact]
+        public void ShouldConvertToUpperCase ()
+        {
+            RotEncryptor("a").Should().Be("N");
+        }
+
+        [Fact]
+        public void ShouldConvertWord()
+        {
+            RotEncryptor("dEf").Should().Be("QRS");
+        }
+
+        [Fact]
+        public void ShouldWrapAroundAlphabet()
+        {
+            RotEncryptor("xyz").Should().Be("KLM");
+        }
+
+        [Fact]
+
+        public void ShouldOnlyConvertLetters()
+        {
+            RotEncryptor("Hello, World").Should().Be("URYYB, JBEYQ");
+        }
+
+        [Fact]
+        public void ShouldConvertUmlautCharactors()
+        {
+            RotEncryptor("Großraumbüroklimagerätestörung").Should().Be("TEBFFENHZOHREBXYVZNTRENRGRFGBREHAT");
+        }
+
+        [Fact]
+        public void ShouldShiftBy2()
+        {
+            RotEncryptor("C", 2).Should().Be("E");
+        }
+
+        [Fact]
+        public void ShouldShiftBy53()
+        {
+            RotEncryptor("A", 53).Should().Be("B");
+        }
+
+        [Fact]
+        public void ShouldShiftDigits()
+        {
+            RotEncryptor("0", 2).Should().Be("2");
+        }
+
+        [Fact]
+        public void ShouldShiftDigitsAroundDigitsEnd()
+        {
+            RotEncryptor("9", 4).Should().Be("3");
+        }
+
+        [Fact]
+        public void ShouldShiftDigitsAndLetters()
+        {
+            RotEncryptor("A3", 2).Should().Be("C5");
+        }
+
+        private string RotEncryptor(string input, int shiftBy = 13)
+        {
+            return new RotEncryptor(shiftBy).Encrypt(input);
+        }
+
+    }
+}
